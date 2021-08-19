c799fe3f9dbafd306a7828bc31bb76b7db50bfb1
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-19 18:28:55 +0100

    exceptions

---
+++ movie/src/main/java/com/example/movie/exception/MovieException.java
@@ -0,0 +1,21 @@
+package com.example.movie.exception;
+
+public class MovieException extends Exception {
+
+    public MovieException(String message){
+        super(message);
+    }
+
+    public MovieException(){
+        super();
+    }
+
+    public MovieException(String message, Throwable ex){
+        super(message, ex);
+    }
+
+    public MovieException(Throwable ex){
+        super(ex);
+    }
+
+}
