ca1e718016d4ed5d11d77885ac442b9297b2f39c
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-19 18:33:34 +0100

    service

---
+++ movie_rating/src/main/java/com/example/movie_rating/service/RatingService.java
@@ -0,0 +1,17 @@
+package com.example.movie_rating.service;
+
+
+import com.example.movie_rating.exceptions.RatingException;
+import com.example.movie_rating.model.Rating;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface RatingService {
+
+    void addRating(Rating rating);
+    Rating getRating(Integer movieId) throws RatingException;
+    List<Rating> getRatings();
+
+}
---
+++ movie_rating/src/main/java/com/example/movie_rating/service/RatingServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.movie_rating.service;
+
+import com.example.movie_rating.exceptions.RatingException;
+import com.example.movie_rating.model.Rating;
+import com.example.movie_rating.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class RatingServiceImpl implements RatingService{
+
+
+    @Autowired
+    RatingRepository ratingRepository;
+
+    @Override
+    public void addRating(Rating rating) {
+        ratingRepository.save(rating);
+    }
+
+    @Override
+    public Rating getRating(Integer movieId) throws RatingException {
+        return ratingRepository.getRating(movieId);
+    }
+
+    @Override
+    public List<Rating> getRatings() {
+        return ratingRepository.findAll();
+    }
+}
