32e73446b6aa94e552e1b4fa5a8273e41fc2bfeb
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-19 18:32:04 +0100

    main app

---
+++ movie_rating/src/main/java/com/example/movie_rating/MovieRatingApplication.java
@@ -0,0 +1,19 @@
+package com.example.movie_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MovieRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRatingApplication.class, args);
+    }
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
