663c3599adee05e7a2bd1392eb7da5ebfacb7c26
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-21 21:23:43 +0100

    update

--- movie/src/main/java/com/example/movie/controller/MovieController.java
+++ movie/src/main/java/com/example/movie/controller/MovieController.java
@@ -2,25 +2,21 @@ package com.example.movie.controller;
 
 import com.example.movie.exception.MovieException;
 import com.example.movie.model.Movie;
-import com.example.movie.model.Rating;
 import com.example.movie.service.MovieService;
 import com.example.movie.service.UpdateMovie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("")
 public class MovieController {
 
     @Autowired
     MovieService movieService;
 
-    @PostMapping("movies/")
+    @PostMapping("movies")
     public void addMovie(@RequestBody Movie movie) {
 
         movieService.addMovie(movie);
@@ -56,26 +52,5 @@ public class MovieController {
         }
     }
 
-    @RequestMapping("default")
-    public List<Movie> getCatalog() {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        List<Rating> ratings = Arrays.asList(
-                new Rating(5, "okay", 1),
-                new Rating(3, "okay", 2)
-        );
-
-        return ratings.stream().map(rating -> {
-            try {
-                return new Movie("Avengers", "poster", "synopsis", 2020);
-            } catch (MovieException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }).collect(Collectors.toList());
-
-
-    }
 
 }
