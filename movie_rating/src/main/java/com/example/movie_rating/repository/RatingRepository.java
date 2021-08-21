be60d3f2e871625f1a9151c52bdc07149abfa703
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-21 21:21:35 +0100

    update

--- movie_rating/src/main/java/com/example/movie_rating/repository/RatingRepository.java
+++ movie_rating/src/main/java/com/example/movie_rating/repository/RatingRepository.java
@@ -3,11 +3,11 @@ package com.example.movie_rating.repository;
 import com.example.movie_rating.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
-    @Query(value = "SELECT AVG(movie_rating.rating) FROM Rating movie_rating WHERE movie_rating.ratingId= ?1", nativeQuery = true)
-    Rating getRating(Integer movieId);
+//    @Query(value = "SELECT avg(movie_rating.rating) FROM Rating movie_rating")
+    @Query(value = "SELECT AVG(rating) FROM rating WHERE movie_id= ?1", nativeQuery = true)
+    Double getRating(Integer movieId);
 }
