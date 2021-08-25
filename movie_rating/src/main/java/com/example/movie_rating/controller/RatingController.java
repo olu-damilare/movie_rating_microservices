3607dc4f93263338635c5e3ff30f13e1f9b09766
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-25 05:15:31 +0100

    added features

--- movie_rating/src/main/java/com/example/movie_rating/controller/RatingController.java
+++ movie_rating/src/main/java/com/example/movie_rating/controller/RatingController.java
@@ -3,10 +3,12 @@ package com.example.movie_rating.controller;
 
 import com.example.movie_rating.exceptions.RatingException;
 import com.example.movie_rating.model.Rating;
+import com.example.movie_rating.model.UserRating;
 import com.example.movie_rating.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -22,15 +24,32 @@ public class RatingController {
     }
 
     @GetMapping("/rating/{movieId}")
-    public Rating getRating(@PathVariable Integer movieId) {
+    public Rating getRating(@PathVariable("movieId") Integer movieId) {
         Rating rating = null;
         try {
             rating = ratingService.getRating(movieId);
         } catch (RatingException e) {
             e.printStackTrace();
         }
-
         return rating;
+    }
+
+
+    @GetMapping("/users/{userId}")
+    public UserRating getRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = null;
+        try {
+            ratings = Arrays.asList(
+                    new Rating(3.0, "okay", 1),
+                    new Rating(5.0, "impressive", 2)
+            );
+        } catch (RatingException e) {
+            e.printStackTrace();
+        }
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
     }
 
     @GetMapping("/ratings")
