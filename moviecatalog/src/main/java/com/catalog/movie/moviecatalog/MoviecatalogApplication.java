164bdeb3bb3e5a2f83027755d2b304bc4e916343
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-06 18:58:53 +0100

    catalog main app

---
+++ moviecatalog/src/main/java/com/catalog/movie/moviecatalog/MoviecatalogApplication.java
@@ -0,0 +1,15 @@
+package com.catalog.movie.moviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MoviecatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviecatalogApplication.class, args);
+    }
+
+}
