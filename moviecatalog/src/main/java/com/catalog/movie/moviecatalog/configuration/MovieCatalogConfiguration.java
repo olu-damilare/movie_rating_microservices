0fbd58af81c73470f6e2cf28588bf483b3acef7c
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-06 18:55:59 +0100

    rest template and web client configuration

---
+++ moviecatalog/src/main/java/com/catalog/movie/moviecatalog/configuration/MovieCatalogConfiguration.java
@@ -0,0 +1,23 @@
+package com.catalog.movie.moviecatalog.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class MovieCatalogConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
+    }
+
+}
