5a5811070a0ae4c44a0235b423b6a172a1d59155
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-06 18:47:04 +0100

    movie controller

--- movie/src/main/java/com/example/movie/controller/MovieController.java
+++ movie/src/main/java/com/example/movie/controller/MovieController.java
@@ -2,19 +2,38 @@ package com.example.movie.controller;
 
 import com.example.movie.exception.MovieException;
 import com.example.movie.model.Movie;
+import com.example.movie.model.MovieSummary;
 import com.example.movie.service.MovieService;
 import com.example.movie.service.UpdateMovie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/movies/")
 public class MovieController {
 
     @Autowired
     MovieService movieService;
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("{movieId}/")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        MovieSummary movieSummary = restTemplate.getForObject(
+            "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey,
+            MovieSummary.class
+        );
+        assert movieSummary != null;
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview(), movieSummary.getRelease_date());
+    }
 
     @PostMapping("movies")
     public void addMovie(@RequestBody Movie movie) {
