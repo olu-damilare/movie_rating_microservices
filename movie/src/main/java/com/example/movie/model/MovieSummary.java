90728bfbe0293140ae611566c87fb4083eb15898
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-06 18:41:17 +0100

    movie summary class

---
+++ movie/src/main/java/com/example/movie/model/MovieSummary.java
@@ -0,0 +1,41 @@
+package com.example.movie.model;
+
+public class MovieSummary {
+
+    private String id;
+    private String title;
+    private String overview;
+    private String release_date;
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+}
