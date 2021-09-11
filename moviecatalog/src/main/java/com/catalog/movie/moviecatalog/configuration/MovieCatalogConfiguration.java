87768d550cb210a2429702eb398f48ade7d021ee
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-11 03:48:00 +0100

    catalog configuration

--- moviecatalog/src/main/java/com/catalog/movie/moviecatalog/configuration/MovieCatalogConfiguration.java
+++ moviecatalog/src/main/java/com/catalog/movie/moviecatalog/configuration/MovieCatalogConfiguration.java
@@ -3,6 +3,7 @@ package com.catalog.movie.moviecatalog.configuration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,7 +13,9 @@ public class MovieCatalogConfiguration {
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
     @Bean
