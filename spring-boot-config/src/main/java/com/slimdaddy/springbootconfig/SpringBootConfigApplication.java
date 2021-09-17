4d29714e72fb25f9be8abc66763664ff93a30608
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-17 03:21:14 +0100

    main app

---
+++ spring-boot-config/src/main/java/com/slimdaddy/springbootconfig/SpringBootConfigApplication.java
@@ -0,0 +1,18 @@
+package com.slimdaddy.springbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
+@SpringBootApplication
+public class SpringBootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigApplication.class, args);
+
+	}
+
+}
